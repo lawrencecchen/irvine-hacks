4ebd146221c8ee970e152e5dd203ce5c4bb225d6
Lawrence Chen <lawrencechen2002@gmail.com>
2019-08-07 07:39:18 -0700

    Add first production build

---
+++ build/precache-manifest.1f83ef792c73fae0950598092b20b023.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4b6d87c3b216f4f58356d74b81ca63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f0f456c661549a174b4",
+    "url": "/static/css/main.97bc0647.chunk.css"
+  },
+  {
+    "revision": "6e3b69fd1b183303c13d",
+    "url": "/static/js/2.4837bf97.chunk.js"
+  },
+  {
+    "revision": "8f0f456c661549a174b4",
+    "url": "/static/js/main.e2df9286.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ba1819cf6733aa28e1471e9de6a01f2d",
+    "url": "/static/media/embrace-the-curve.ba1819cf.svg"
+  }
+]);
