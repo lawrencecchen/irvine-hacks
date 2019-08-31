3667381279abf351f204cad250fac7cb24fdbf90
Lawrence Chen <lawrencechen2002@gmail.com>
2019-08-31 14:11:33 -0700

    Fix footer spacing on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,10 @@ const Footer = () => {
     <Container fluid className="p-0">
       <Jumbotron className="m-0 bg-nav-dark text-light rounded-0">
         <Row>
-          <Col></Col>
           <Col className="text-center">&copy; Irvine Hacks 2019</Col>
-          <Col></Col>
         </Row>
-        <Row> </Row>
         <Row>
-          <Col></Col>
           <Col className="text-center">Website designed and developed by Lawrence Chen</Col>
-          <Col></Col>
         </Row>
       </Jumbotron>
     </Container>
