fbad486966af4abef5364391666aed914447383a
lawrencecchen <md.lawrence.chen@gmail.com>
2019-10-17 20:28:52 -0700

    feat: update schedule

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -19,24 +19,16 @@ const TimeList = (props) => {
 const scheduleInfo = {
   Saturday: [
     ['9:00 AM', 'Check-In'],
-    ['11:00 AM', 'Opening Ceremony; Keynote opening speech*'],
-    ['11:00 AM', 'Hacking Begins; Team Formation'],
-    ['12:00 AM', 'Lunch'],
-    ['1:00 PM', 'Workshop #1*'],
-    ['2:30 PM', 'Workshop #2*'],
-    ['4:00 PM', 'Workshop #3*'],
+    ['9:15 AM', 'Opening Ceremony; Keynote opening speech*'],
+    ['9:30 AM', 'Hacking Begins; Team Formation'],
+    ['9:45 AM', 'Workshop #1*'],
+    ['11:00 PM', 'Workshop #2*'],
+    ['12:15 PM', 'Lunch'],
+    ['1:00 PM', 'Workshop #3*'],
     ['6:00 PM', 'Dinner'],
-    ['8:00 PM', 'Workshop #4*']
-  ],
-  Sunday: [
-    ['12:00 AM', 'Midnight Snack'],
-    ['2:00 AM', 'Game Break *'],
-    ['7:00 AM', 'Breakfast'],
-    ['9:00 AM', 'Project Submission on DevPost'],
-    ['10:00 AM', 'Hacking Ends'],
-    ['10:15 AM', 'Judging(Presentation Style) '],
-    ['11:40 AM', 'Closing Ceremony'],
-    ['12:00 AM', 'Event Ends']
+    ['9:30 PM', 'Project Submission on DevPost; Hacking Ends'],
+    ['9:45 PM', 'Closing Ceremony'],
+    ['10:00 PM', 'Event Ends'],
   ]
 }
 
@@ -48,21 +40,15 @@ const Schedule = React.forwardRef((props, ref) => (
     </Header>
     <Row>
       <Col></Col>
-      <Col lg={5} className="mb-3">
+      <Col lg={10} className="mb-3">
         <ListGroup>
           <ListGroup.Item variant="primary">Saturday</ListGroup.Item>
           <TimeList timestamps={scheduleInfo.Saturday} />
         </ListGroup>
       </Col>
-      <Col lg={5}>
-        <ListGroup>
-          <ListGroup.Item variant="primary">Sunday</ListGroup.Item>
-          <TimeList timestamps={scheduleInfo.Sunday}></TimeList>
-        </ListGroup>
-      </Col>
       <Col></Col>
     </Row>
-  </Container >
+  </Container>
 ));
 
 export default Schedule;
