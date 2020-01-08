c10aeff7bc43b028bbf8a7d3dc2bc56188f46111
lawrencecchen <md.lawrence.chen@gmail.com>
2020-01-07 16:44:11 -0800

    Add Balsamiq to sponsors

--- src/pages/Sponsors.js
+++ src/pages/Sponsors.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { Container, Jumbotron } from 'react-bootstrap';
-import Header from '../components/Header';
-import EmailHyperlink from '../components/EmailHyperlink';
-import ImageLink from '../components/ImageLink';
-import HackClubBankLight from '../media/sponsors/HackClubBankLight.png';
+import React from "react";
+import { Container, Jumbotron } from "react-bootstrap";
+import Header from "../components/Header";
+import EmailHyperlink from "../components/EmailHyperlink";
+import ImageLink from "../components/ImageLink";
+import HackClubBankLight from "../media/sponsors/HackClubBankLight.png";
+import Balsamiq from "../media/sponsors/balsamiq-logo-screen.svg";
 
 const Sponsors = React.forwardRef((props, ref) => (
   <Container fluid className="p-0 m-0" ref={ref}>
-    <Header>
-      Sponsors
-    </Header>
+    <Header>Sponsors</Header>
     <Container className="d-flex justify-content-center mb-4">
-      <ImageLink href="https://hackclub.com/bank/" url={HackClubBankLight} />
+      <div class="row">
+        <div class="col-md mt-3 text-center">
+          <ImageLink
+            href="https://hackclub.com/bank/"
+            url={HackClubBankLight}
+          />
+        </div>
+        <div class="col-md mt-3 text-center">
+          <ImageLink href="https://balsamiq.com/" url={Balsamiq} />
+        </div>
+      </div>
     </Container>
     <Jumbotron className="bg-light rounded-0">
-      <p style={{ fontSize: '1.3em' }}>
-        Want to sponsor us? Check out our sponsorship prospectus and email us at <EmailHyperlink>sponsors@irvinehacks.com</EmailHyperlink>
+      <p style={{ fontSize: "1.3em" }}>
+        Want to sponsor us? Check out our sponsorship prospectus and email us at{" "}
+        <EmailHyperlink>sponsors@irvinehacks.com</EmailHyperlink>
       </p>
     </Jumbotron>
   </Container>
