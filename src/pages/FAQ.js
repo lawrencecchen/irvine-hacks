b7dfc340c97534062649d11257108bd82bde87b9
lawrencecchen <md.lawrence.chen@gmail.com>
2020-01-12 21:16:38 -0800

    Change FAQ from 24 to 12 hours.

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import { Container, Card } from 'react-bootstrap';
-import '../components/Cards.scss';
-import Header from '../components/Header';
-import Banner from '../media/IrvineHacksTransparentLogo.png'
+import React from "react";
+import { Container, Card } from "react-bootstrap";
+import "../components/Cards.scss";
+import Header from "../components/Header";
+import Banner from "../media/IrvineHacksTransparentLogo.png";
 
 const FAQ = React.forwardRef((props, ref) => (
   <Container fluid className="px-0 mx-0 bg-transparent" ref={ref}>
-    <div style={
-      {
+    <div
+      style={{
         background: `url(${Banner})`,
-        filter: 'blur(8px) brightness(85%)',
-        height: '100%',
-        width: '100%',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        position: 'absolute',
+        filter: "blur(8px) brightness(85%)",
+        height: "100%",
+        width: "100%",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        position: "absolute",
         zIndex: -1,
-        transform: 'translateY(-100px)'
-      }
-    }></div>
+        transform: "translateY(-100px)"
+      }}
+    ></div>
     <Header>FAQ</Header>
     <Container id="responsive-cards" className="my-0 mx-auto py-0 px-1">
       <Card>
         <Card.Body>
           <Card.Title>What is a Hackathon?</Card.Title>
           <Card.Text>
-            Irvine Hacks gathers high school students together for 24 hours to collaborate and “hack”! Participants design, execute, and present a project based around a theme, winning cool prizes and having fun in the process!
+            Irvine Hacks gathers high school students together for 12 hours to
+            collaborate and “hack”! Participants design, execute, and present a
+            project based around a theme, winning cool prizes and having fun in
+            the process!
           </Card.Text>
         </Card.Body>
       </Card>
@@ -34,7 +37,9 @@ const FAQ = React.forwardRef((props, ref) => (
         <Card.Body>
           <Card.Title>Hacking? Is that legal?</Card.Title>
           <Card.Text>
-            Hackathons do not necessarily deal with breaching computer systems and exploiting vulnerabilities. Instead, the word is being redefined into creating unique and innovative projects within a time period.
+            Hackathons do not necessarily deal with breaching computer systems
+            and exploiting vulnerabilities. Instead, the word is being redefined
+            into creating unique and innovative projects within a time period.
           </Card.Text>
         </Card.Body>
       </Card>
@@ -44,27 +49,25 @@ const FAQ = React.forwardRef((props, ref) => (
         target="_blank"
         className="p-0 border-5"
       >
-        <Card.Body style={
-          {
-            backgroundImage: 'url(https://images.wsj.net/im-112234/IM)',
-            backgroundSize: 'cover',
-            filter: 'blur(20)',
-            borderRadius: '3px'
-          }
-        }>
-          <Card.Title
-            style={{ zIndex: 1 }}
-            className="text-light"
-          >
+        <Card.Body
+          style={{
+            backgroundImage: "url(https://images.wsj.net/im-112234/IM)",
+            backgroundSize: "cover",
+            filter: "blur(20)",
+            borderRadius: "3px"
+          }}
+        >
+          <Card.Title style={{ zIndex: 1 }} className="text-light">
             Check out Wall Street Journal's article about "hacking"!
-           </Card.Title>
+          </Card.Title>
         </Card.Body>
       </Card>
       <Card>
         <Card.Body>
           <Card.Title>How much does it cost?</Card.Title>
           <Card.Text>
-            $0 - Registration and attendance are totally free! Meals, snacks, food, drinks, and workshops included.
+            $0 - Registration and attendance are totally free! Meals, snacks,
+            food, drinks, and workshops included.
           </Card.Text>
         </Card.Body>
       </Card>
@@ -72,7 +75,8 @@ const FAQ = React.forwardRef((props, ref) => (
         <Card.Body>
           <Card.Title>When and Where?</Card.Title>
           <Card.Text>
-            Location and time are to be announced! Please check back later for details.
+            Location and time are to be announced! Please check back later for
+            details.
           </Card.Text>
         </Card.Body>
       </Card>
@@ -88,7 +92,8 @@ const FAQ = React.forwardRef((props, ref) => (
         <Card.Body>
           <Card.Title>What if I can't code?</Card.Title>
           <Card.Text>
-            Absolutely no programming experience is necessary! Workshops will be open throughout the day to teach basic coding skills, though!
+            Absolutely no programming experience is necessary! Workshops will be
+            open throughout the day to teach basic coding skills, though!
           </Card.Text>
         </Card.Body>
       </Card>
@@ -96,12 +101,16 @@ const FAQ = React.forwardRef((props, ref) => (
         <Card.Body>
           <Card.Title>How can I become a sponsor?</Card.Title>
           <Card.Text>
-            Check out our sponsorship prospectus, and contact us at <a href="mailto:sponsors@irvinehacks.com">sponsors@irvinehacks.com</a>.
+            Check out our sponsorship prospectus, and contact us at{" "}
+            <a href="mailto:sponsors@irvinehacks.com">
+              sponsors@irvinehacks.com
+            </a>
+            .
           </Card.Text>
         </Card.Body>
       </Card>
     </Container>
-  </Container >
+  </Container>
 ));
 
 export default FAQ;
